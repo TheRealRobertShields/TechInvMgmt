93ca334e7fd8d2ce8edbac833eef3f45f148fd3a
Robert Shields <roberthshields93@yahoo.com>
2020-04-23 20:55:45 -0700

    Add project files.

---
+++ TechInvMgmt/Models/IdentityHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechInvMgmt.Models
+{
+    public class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions idOptions)
+        {
+            // Set sign in options
+            idOptions.SignIn.RequireConfirmedAccount = false;
+            idOptions.SignIn.RequireConfirmedEmail = false;
+            idOptions.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // Set password options
+            idOptions.Password.RequiredLength = 8;
+            idOptions.Password.RequireNonAlphanumeric = false;
+
+            // Set lockout options
+            idOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+            idOptions.Lockout.MaxFailedAccessAttempts = 5;
+        }
+    }
+}

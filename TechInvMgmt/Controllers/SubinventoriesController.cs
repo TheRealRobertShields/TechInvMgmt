05cb9a898fcb189245aaeabd82d4bf328f421fdf
Robert Shields <roberthshields93@yahoo.com>
2020-04-27 22:04:02 -0700

    Finished assigning methods to roles

--- TechInvMgmt/Controllers/SubinventoriesController.cs
+++ TechInvMgmt/Controllers/SubinventoriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using TechInvMgmt.Models;
 
 namespace TechInvMgmt.Controllers
 {
+    [Authorize(Roles = "ISP, FSM, Admin")]
     public class SubinventoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -20,6 +22,7 @@ namespace TechInvMgmt.Controllers
         }
 
         // GET: Subinventories
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Subinventories.ToListAsync());
